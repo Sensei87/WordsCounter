f2c9c23a864c36ee4401acdc467f069afe9ff9b5
Sergey Povarckuk <s.povarchuk@gmail.com>
2022-03-15 14:03:37 +0200

    Changed ParserTests class

--- WordsCounter.Tests/ParserTests.cs
+++ WordsCounter.Tests/ParserTests.cs
@@ -14,9 +14,16 @@ namespace WordsCounter.Tests
         public void CheckPars()
         {
             // Arrange
-            var text = "Every time you call the SaveChanges method, an implicit transaction is started so that if " +
-                "something goes wrong, it would automatically roll back all the changes. If the multiple changes" +
-                "within the transaction succeed, then the transaction and all changes are committed";
+            var text = "Split PDF document online is a web service that allows you to" +
+                " split your PDF document into separate pages. This simple application" +
+                " has several modes of operation, you can split your PDF document into separate pages," +
+                " i.e. each page of the original document will be a separate PDF document," +
+                " you can split your document into even and odd pages, this function will " +
+                "come in handy if you need to print a document in the form of a book, " +
+                "you can also specify page numbers in the settings and the Split PDF" +
+                " application will create separate PDF documents only with these pages" +
+                " and the fourth mode of operation allows you to create a new PDF document " +
+                "in which there will be only those pages that you specified";
 
             var _parser = new Parser();
             var list = new List<string>();
@@ -25,58 +32,55 @@ namespace WordsCounter.Tests
             var pars =_parser.Pars(text);
             pars.Add(text);
             list.Add(text);
-            list.Sort();
+            
 
             // Assert
             Assert.Equal(list, pars);
         }
+        
         [Fact]
-        public void CheckNumbers()
+        public void CheckOneNumbers()
         {
             // Arrange
-            var text = "Every time you call the SaveChanges method, an implicit transaction is started so that if " +
-               "something goes wrong, it would automatically roll back all the changes. If the multiple changes" +
-               "within the transaction succeed, then the transaction and all changes are committed";
+            var text = "Split PDF document online is a web service that allows you to" +
+                " split your PDF document into separate pages. This simple application" +
+                " has several modes of operation, you can split your PDF document into separate pages," +
+                " i.e. each page of the original document will be a separate PDF document," +
+                " you can split your document into even and odd pages, this function will " +
+                "come in handy if you need to print a document in the form of a book, " +
+                "you can also specify page numbers in the settings and the Split PDF" +
+                " application will create separate PDF documents only with these pages" +
+                " and the fourth mode of operation allows you to create a new PDF document " +
+                "in which there will be only those pages that you specified"; 
 
             var _parser = new Parser();
             var list = new List<string>();
-            var sorted = new SortedList<string, int>(StringComparer.OrdinalIgnoreCase);
 
             // Act
-            var pars = _parser.OneMutchesOfWord(text);
-           
+            var oneNumber = _parser.OneMutchesOfWord(text);
+            var result = "";
+            string[] source = text.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',' },
+                    StringSplitOptions.RemoveEmptyEntries);
 
-            foreach (var item in text.Split())
+            var finalPercent = 0.0;
+            int counter = 1;
+            for (int i = 0; i < source.Length; i++)
             {
-                if (sorted.ContainsKey(item))
-                {
-                    sorted[item]++;
-                }
-                else
-                {
-                    sorted.Add(item, 1);
-                }
-
+                var match = from word in source
+                            where word
+        .ToLowerInvariant() == source[i].ToLowerInvariant()
+                            select word;
+                finalPercent = match.Count() * 100.0 / source.Length;
+                finalPercent = Math.Round(finalPercent, 2);
+                result += "\n " + counter + ". " + source[i] + " [" + match.Count() + " words] (" + finalPercent + " %)";
+                counter++;
             }
-            
+            list.Add(result);
 
-            int[] counter = new int[sorted.Capacity];
-            int index = 0;
-            var finalPercent = 0M;
+            // Arrange
+            Assert.Equal(list, oneNumber);
 
-            foreach (var item in sorted)
-            {
-                if (item.Value == 0 || item.Value > 10) continue;
-                counter[index] += item.Value;
-                finalPercent = counter[index] * 100M / sorted.Capacity;
-                index++;
 
-                list.Add(finalPercent + "%  " + item.Key + "  " + item.Value);
-
-            }
-
-            // Assert
-            Assert.Equal(list, pars);
 
 
         }
